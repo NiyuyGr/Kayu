75b916a471674c8d45d6e6fc1ada7bab8706aa8b
DanAurMG <raizeremi.15.damg@gmail.com>
2023-06-02 09:52:46 -0600

    DAMG: Implementé el uso de router Cree y terminé cada css para la nav y el footer

--- StaticPages/reactCourse/src/App.jsx
+++ StaticPages/reactCourse/src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 import Landing from './components/Landing'
 import Login from './components/Login'
-
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   
   //<Landing />
   
   return (
-    <div>
-      <Login />
-    </div>
+    <>    
+    <Routes>
+      <Route path="/" element = {<Landing />} />
+      <Route path="/Login" element = {<Login />} />
+    </Routes>
+
+    </>
   )
 }
 
